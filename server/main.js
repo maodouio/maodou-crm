7b4f1282d2332eb240f4b1ad321abb99afd7d518
limingth <limingth@gmail.com>
2016-05-07 13:22:25 +0800

    Add roles of 'admin'/'user'

--- server/main.js
+++ server/main.js
@@ -3,3 +3,29 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Accounts.onCreateUser(function(options, user) {
+  console.log('--- onCreateUser ---');
+  console.log(options);
+  console.log(user);
+  console.log(user._id);
+
+  return user;
+});
+
+Accounts.onLogin(function(user){
+  console.log('--- onLogin ---');
+  console.log(user);
+
+  var userNumber = Meteor.users.find().count();
+  console.log('userNumber = ', userNumber);
+
+  if (userNumber === 1) {
+    console.log('The first signup user is admin by default');
+    Roles.addUsersToRoles(user.user._id, ['admin']);
+  }
+
+  // Every user has role of 'user' by default
+  Roles.addUsersToRoles(user.user._id, ['user']);
+
+});
